3915f5816744b4897622f70795af68d18d3bfc0c
Juliane Turzynski <juliane.turzynski@t-systems.com>
2024-05-06 20:12:16 +0200

    Update pydantic

--- starter/main.py
+++ starter/main.py
@@ -1,9 +1,8 @@
-from dataclasses import Field
 from fastapi import Body, FastAPI
 import joblib
 import numpy as np
 import pandas as pd
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from starter.starter.ml.data import process_data
 
@@ -16,20 +15,20 @@ def hyphen_to_underscore(field_name):
 
 
 class Data(BaseModel):
-    age: int = Field(..., example=45)
-    capital_gain: int = Field(..., example=2174)
-    capital_loss: int = Field(..., example=0)
-    education: str = Field(..., example="Bachelors")
-    education_num: int = Field(..., example=13)
-    fnlgt: int = Field(..., example=2334)
-    hours_per_week: int = Field(..., example=60)
-    marital_status: str = Field(..., example="Never-married")
-    native_country: str = Field(..., example="Cuba")
-    occupation: str = Field(..., example="Prof-specialty")
-    race: str = Field(..., example="Black")
-    relationship: str = Field(..., example="Wife")
-    sex: str = Field(..., example="Female")
-    workclass: str = Field(..., example="State-gov")
+    age: int = Field(default=45)
+    capital_gain: int = Field(default=2174)
+    capital_loss: int = Field(default=0)
+    education: str = Field(default="Bachelors")
+    education_num: int = Field(default=13)
+    fnlgt: int = Field(default=2334)
+    hours_per_week: int = Field(default=60)
+    marital_status: str = Field(default="Never-married")
+    native_country: str = Field(default="Cuba")
+    occupation: str = Field(default="Prof-specialty")
+    race: str = Field(default="Black")
+    relationship: str = Field(default="Wife")
+    sex: str = Field(default="Female")
+    workclass: str = Field(default="State-gov")
 
     class Config:
             alias_generator = hyphen_to_underscore
