8d21e2ec4ab7743a5231c470b4eb0aeca316f38f
Juliane Turzynski <juliane.turzynski@t-systems.com>
2024-05-06 17:32:06 +0200

    Reformat code

--- starter/tests/starter/ml/test_model.py
+++ starter/tests/starter/ml/test_model.py
@@ -1,6 +1,11 @@
+
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from starter.starter.ml.model import train_model, compute_model_metrics, inference
+from starter.starter.ml.model import (
+    train_model,
+    compute_model_metrics,
+    inference
+)
 
 
 def test_train_model():
